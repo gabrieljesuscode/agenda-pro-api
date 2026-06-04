36be3c27be7fefde148e221f9d65d831043d6b51
GabrielJesus0 <gabriel053jesus@gmail.com>
2026-06-04 15:52:07 -0300

    feat: user register with password hashing

--- src/shared/services/auth/register/index.ts
+++ src/shared/services/auth/register/index.ts
@@ -1,4 +1,5 @@
 import { prisma } from '../../../../database/prisma/prisma';
+import * as bcrypt from 'bcrypt';
 
 export interface UserRegisterDTO {
   name: string
@@ -7,7 +8,30 @@ export interface UserRegisterDTO {
 }
 
 export const register = async (data: UserRegisterDTO) => {
+
+  // const userRegistered = await prisma.user.findFirst({
+  //   where: {
+  //     email: data.email
+  //   }
+  // });
+
+  // if (userRegistered) return undefined;
+
+  const saltRounds = 10;
+
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  if (!salt) return undefined;
+
+  const hash = await bcrypt.hash(data.password, salt);
+
+  if (!hash) return undefined;
+
   return await prisma.user.create({
-    data,
+    data: {
+      name: data.name,
+      email: data.email,
+      password: hash
+    },
   });
 };
