b4defb88a534889d93ad27f01740bf43ae0de0e0
GabrielJesus0 <gabriel053jesus@gmail.com>
2026-06-08 13:21:56 -0300

    feat: comments in route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,9 +6,14 @@ import { DashboardRouter } from './dashboard';
 
 const router = Router();
 
+// Endpoints
+// Rota de Clientes
 router.use(ClientsRouter);
+// Rota de autenticação
 router.use(AuthRouter);
+// Rota de agendamento
 router.use(AppointmentsRouter);
+// Rota de dashboard
 router.use(DashboardRouter);
 
 export { router };
