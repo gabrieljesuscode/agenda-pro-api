80bc2ad402c6237e382d7baa582840c875ee7d11
GabrielJesus0 <gabriel053jesus@gmail.com>
2026-07-03 07:57:37 -0300

    feat: pages and limit and filter get clients

--- src/controllers/clients/list/index.ts
+++ src/controllers/clients/list/index.ts
@@ -4,18 +4,24 @@ import * as z from 'zod';
 import { ClientsService } from '../../../shared/services';
 
 const querySchema = z.object({
-  page: z.number().gt(0).optional(),
-  limit: z.number().gt(0).optional(),
+  page: z.coerce.number().gt(0).optional(),
+  limit: z.coerce.number().gt(0).optional(),
   filter: z.string().optional()
 });
+
+type TQuerySchema = Partial<{
+  page: number
+  limit: number
+  filter: string
+}>;
 
 export const listValidation = validation({
   query: querySchema
 });
 
-export const list: RequestHandler = async (_, res) => {
+export const list: RequestHandler<unknown, unknown, unknown, TQuerySchema> = async (req, res) => {
 
-  const clientsList = await ClientsService.list(res.locals.userId);
+  const clientsList = await ClientsService.list(res.locals.userId, req.query);
 
   return res.status(200).json(clientsList);
 };  
--- src/shared/services/clients/list.ts
+++ src/shared/services/clients/list.ts
@@ -1,9 +1,54 @@
 import { prisma } from '../../../database/prisma/prisma';
+import type { Prisma } from '../../../generated/prisma/client';
 
-export const list = async (id: string) => {
-  return await prisma.client.findMany({
-    where: {
-      userId: id
-    }
+type TQuerySchema = Partial<{
+  page: number
+  limit: number
+  filter: string
+}>;
+
+
+export const list = async (userId: string, query: TQuerySchema) => {
+
+  const { page = 1, limit = 10, filter } = query;
+
+  const pageNumber = Number(page);
+  const limitNumber = Number(limit);
+
+  const where: Prisma.ClientWhereInput = {
+    userId,
+  };
+
+  if (filter) {
+    where.name = {
+      contains: filter,
+      mode: 'insensitive',
+    };
+  }
+
+  const clients = await prisma.client.findMany({
+    where,
+    omit: {
+      userId: true,
+      createdAt: true,
+      updatedAt: true
+    },
+    skip: (pageNumber - 1) * limitNumber,
+    take: limitNumber
   });
+
+  const total = await prisma.client.count({
+    where,
+  });
+
+  const totalPages = Math.ceil(total / limit);
+
+  return {
+    page: pageNumber,
+    limit: limitNumber,
+    filter: filter ? filter : null,
+    total,
+    totalPages,
+    clients
+  };
 };
